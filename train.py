98f4e0e71fd474e13813f5172a81cb1a23d0d192
aliaksandr siarohin <aliaksandr.siarohin@gmail.com>
2019-10-09 16:47:49 +0200

    Add equivariance loss.

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import transforms as T
 from tqdm import tqdm
@@ -10,6 +11,64 @@ from tqdm import tqdm
 from ab_dataset import ABDataset
 from network import Generator, Discriminator
 from sync_batchnorm import DataParallelWithCallback
+from util import make_coordinate_grid
+
+
+class Transform:
+    def __init__(self, bs, **kwargs):
+        noise = torch.normal(mean=0, std=kwargs['sigma_affine'] * torch.ones([bs, 2, 3]))
+        self.theta = noise + torch.eye(2, 3).view(1, 2, 3)
+
+        if 'flips' in kwargs and kwargs['flips']:
+            flips = (torch.randint(2, dtype=self.theta.type(), size=self.theta.shape) - 0.5) * 2
+            self.theta = self.theta * flips
+
+        self.bs = bs
+
+        if ('sigma_tps' in kwargs) and ('points_tps' in kwargs):
+            self.tps = True
+            self.control_points = make_coordinate_grid((kwargs['points_tps'], kwargs['points_tps']), type=noise.type())
+            self.control_points = self.control_points.unsqueeze(0)
+            self.control_params = torch.normal(mean=0,
+                                               std=kwargs['sigma_tps'] * torch.ones([bs, 1, kwargs['points_tps'] ** 2]))
+        else:
+            self.tps = False
+
+    def transform_frame(self, frame):
+        grid = make_coordinate_grid(frame.shape[2:], type=frame.type()).unsqueeze(0)
+        grid = grid.view(1, frame.shape[2] * frame.shape[3], 2)
+        grid = self.warp_coordinates(grid).view(self.bs, frame.shape[2], frame.shape[3], 2)
+        return F.grid_sample(frame, grid, padding_mode="reflection")
+
+    def warp_coordinates(self, coordinates):
+        theta = self.theta.type(coordinates.type())
+        theta = theta.unsqueeze(1)
+        transformed = torch.matmul(theta[:, :, :, :2], coordinates.unsqueeze(-1)) + theta[:, :, :, 2:]
+        transformed = transformed.squeeze(-1)
+
+        if self.tps:
+            control_points = self.control_points.type(coordinates.type())
+            control_params = self.control_params.type(coordinates.type())
+            distances = coordinates.view(coordinates.shape[0], -1, 1, 2) - control_points.view(1, 1, -1, 2)
+            distances = torch.abs(distances).sum(-1)
+
+            result = distances ** 2
+            result = result * torch.log(distances + 1e-6)
+            result = result * control_params
+            result = result.sum(dim=2).view(self.bs, coordinates.shape[1], 1)
+            transformed = transformed + result
+
+        return transformed
+
+
+def normalized_l1(a, b):
+    a = a.view(a.shape[0], -1)
+    b = b.view(b.shape[0], -1)
+
+    a = a / torch.norm(a, dim=1, keepdim=True)
+    b = b / torch.norm(b, dim=1, keepdim=True)
+
+    return torch.abs(a - b).sum()
 
 
 class Trainer:
@@ -102,6 +161,7 @@ class Trainer:
         loader = DataLoader(self.dataset, batch_size=self.config['bs'], shuffle=False,
                             drop_last=True, num_workers=4)
         images_fixed = None
+
         for self.epoch in tqdm(range(self.epoch, self.config['num_epochs'])):
             loss_dict = defaultdict(lambda: 0.0)
             iteration_count = 1
@@ -111,6 +171,7 @@ class Trainer:
 
                 if images_fixed is None:
                     images_fixed = {'A': images_A, 'B': images_B}
+                    transform_fixed = Transform(images_A.shape[0], **self.config['transform_params'])
 
                 if self.config['identity_loss_weight'] != 0:
                     images_trg = self.generatorB(images_B, source=False)
@@ -144,7 +205,25 @@ class Trainer:
                     generator_loss += adversarial_loss
                     loss_dict['adversarial_loss_A'] += adversarial_loss.detach().cpu().numpy()
 
-                if self.BtoA and self.config['cycle_loss_weight'] != 0:
+                if self.config['equivariance_loss_weight_generator'] != 0:
+                    transform = Transform(images_generatedB.shape[0], **self.config['transform_params'])
+                    images_A_transformed = transform.transform_frame(images_A)
+                    loss = normalized_l1(self.generatorB(images_A_transformed, source=True), images_generatedB)
+                    loss = self.config['equivariance_loss_weight_generator'] * loss
+                    generator_loss += loss
+
+                    loss_dict['equivariance_loss_weight_generator_B'] += loss.detach().cpu().numpy()
+
+                if self.config['equivariance_loss_weight_generator'] != 0 and self.BtoA:
+                    transform = Transform(images_generatedA.shape[0], **self.config['transform_params'])
+                    images_B_transformed = transform.transform_frame(images_B)
+                    loss = normalized_l1(self.generatorA(images_B_transformed, source=True), images_generatedA)
+                    loss = self.config['equivariance_loss_weight_generator'] * loss
+                    generator_loss += loss
+
+                    loss_dict['equivariance_loss_weight_generator_A'] += loss.detach().cpu().numpy()
+
+                if self.BtoA and self.config['cycle_loss_weight'] != 0 and self.BtoA:
                     images_cycled = self.generatorA(images_generatedB, source=True)
                     cycle_loss = torch.abs(images_cycled - images_A).mean()
                     cycle_loss = self.config['cycle_loss_weight'] * cycle_loss
@@ -176,10 +255,27 @@ class Trainer:
                 loss_dict['fake_loss_B'] += loss_fake.detach().cpu().numpy()
                 loss_dict['real_loss_B'] += loss_real.detach().cpu().numpy()
 
-                (loss_fake + loss_real).backward()
+                discriminator_loss = (loss_fake + loss_real)
+
+                if self.config['equivariance_loss_weight_discriminator'] != 0:
+                    images_join = torch.cat([images_generatedB.detach(), images_B])
+                    logits_join = torch.cat([logits_fake, logits_real])
+
+                    transform = Transform(images_join.shape[0], **self.config['transform_params'])
+                    images_transformed = transform.transform_frame(images_join)
+                    loss = normalized_l1(self.discriminatorB(images_transformed), logits_join)
+
+                    loss = self.config['equivariance_loss_weight_discriminator'] * loss
+                    discriminator_loss += loss
+                    loss_dict['equivariance_loss_weight_discriminator_B'] += loss.detach().cpu().numpy()
+
+                discriminator_loss.backward()
+
                 self.optimizer_discriminatorB.step()
                 self.optimizer_discriminatorB.zero_grad()
                 self.optimizer_generatorB.zero_grad()
+
+                loss_dict['equivariance_loss_weight_generator_B'] += loss.detach().cpu().numpy()
 
                 if self.BtoA:
                     logits_fake = self.discriminatorA(images_generatedA.detach())
@@ -190,45 +286,60 @@ class Trainer:
                     loss_dict['fake_loss_A'] += loss_fake.detach().cpu().numpy()
                     loss_dict['real_loss_A'] += loss_real.detach().cpu().numpy()
 
-                    (loss_fake + loss_real).backward()
+                    discriminator_loss = (loss_fake + loss_real)
+
+                    if self.config['equivariance_loss_weight_discriminator'] != 0:
+                        images_join = torch.cat([images_generatedA.detach(), images_A])
+                        logits_join = torch.cat([logits_fake, logits_real])
+
+                        transform = Transform(images_join.shape[0], **self.config['transform_params'])
+                        images_transformed = transform.transform_frame(images_join)
+                        loss = normalized_l1(self.discriminatorA(images_transformed), logits_join)
+
+                        loss = self.config['equivariance_loss_weight_discriminator'] * loss
+                        discriminator_loss += loss
+                        loss_dict['equivariance_loss_weight_discriminator_B'] += loss.detach().cpu().numpy()
+
+                    discriminator_loss.backward()
                     self.optimizer_discriminatorA.step()
                     self.optimizer_discriminatorA.zero_grad()
                     self.optimizer_generatorA.zero_grad()
 
-                iteration_count += 1
+            iteration_count += 1
 
-            with torch.no_grad():
-                if not self.BtoA:
-                    self.generatorB.eval()
-                    self.logger.save_images(self.epoch, images_fixed['A'],
-                                            self.generatorB(images_fixed['A'], source=True),
-                                            images_fixed['A'].permute(0, 1, 3, 2),
-                                            self.generatorB(images_fixed['A'].permute(0, 1, 3, 2), source=True))
-                    self.generatorB.train()
-                else:
-                    self.generatorA.eval()
-                    self.generatorB.eval()
+        with torch.no_grad():
+            if not self.BtoA:
+                self.generatorB.eval()
+                transformed = transform_fixed.transform_frame(images_fixed['A'])
+                self.logger.save_images(self.epoch, images_fixed['A'],
+                                        self.generatorB(images_fixed['A'], source=True),
+                                        transformed, self.generatorB(transformed, source=True))
+                self.generatorB.train()
+            else:
+                self.generatorA.eval()
+                self.generatorB.eval()
 
-                    images_generatedB = self.generatorB(images_fixed['A'], source=True)
-                    images_generatedA = self.generatorA(images_fixed['B'], source=True)
-                    self.logger.save_images(self.epoch,
-                                            images_fixed['A'], images_generatedB,
-                                            images_fixed['A'].permute(0, 1, 3, 2),
-                                            self.generatorB(images_fixed['A'].permute(0, 1, 3, 2), source=True),
-                                            self.generatorA(images_generatedB, source=True), images_fixed['B'],
-                                            images_generatedA, self.generatorB(images_generatedA, source=True))
+                images_generatedB = self.generatorB(images_fixed['A'], source=True)
+                images_generatedA = self.generatorA(images_fixed['B'], source=True)
 
-                    self.generatorA.train()
-                    self.generatorB.train()
+                transformed = transform_fixed.transform_frame(images_fixed['A'])
+                self.logger.save_images(self.epoch,
+                                        images_fixed['A'], images_generatedB,
+                                        transformed, self.generatorB(transformed, source=True),
+                                        self.generatorA(images_generatedB, source=True), images_fixed['B'],
+                                        images_generatedA, self.generatorB(images_generatedA, source=True))
 
-            self.scheduler_generatorB.step()
-            self.scheduler_discriminatorB.step()
-            if self.BtoA:
-                self.scheduler_generatorA.step()
-                self.scheduler_discriminatorA.step()
+                self.generatorA.train()
+                self.generatorB.train()
 
-            save_dict = {key: value / iteration_count for key, value in loss_dict.items()}
-            save_dict['lr'] = self.optimizer_generatorB.param_groups[0]['lr']
+        self.scheduler_generatorB.step()
+        self.scheduler_discriminatorB.step()
+        if self.BtoA:
+            self.scheduler_generatorA.step()
+            self.scheduler_discriminatorA.step()
 
-            self.logger.log(self.epoch, save_dict)
-            self.save()
+        save_dict = {key: value / iteration_count for key, value in loss_dict.items()}
+        save_dict['lr'] = self.optimizer_generatorB.param_groups[0]['lr']
+
+        self.logger.log(self.epoch, save_dict)
+        self.save()
--- util.py
+++ util.py
@@ -1,3 +1,24 @@
+import torch
+
 def denorm(x):
     out = (x + 1) / 2
     return out.clamp_(0, 1)
+
+
+def make_coordinate_grid(spatial_size, type):
+    """
+    Create a meshgrid [-1,1] x [-1,1] of given spatial_size.
+    """
+    h, w = spatial_size
+    x = torch.arange(w).type(type)
+    y = torch.arange(h).type(type)
+
+    x = (2 * (x / (w - 1)) - 1)
+    y = (2 * (y / (h - 1)) - 1)
+
+    yy = y.view(-1, 1).repeat(1, w)
+    xx = x.view(1, -1).repeat(h, 1)
+
+    meshed = torch.cat([xx.unsqueeze_(2), yy.unsqueeze_(2)], 2)
+
+    return meshed
